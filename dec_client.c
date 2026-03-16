fb2a4ba2e58d7ec0c78c100105b298db806ff733
Sheryll Herman Hernandez <hernashe@oregonstate.edu>
2026-03-16 02:52:16 +0000

    finish otp assignment

---
+++ dec_client.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+#define MAX_BUF 100000
+
+void strip_newline(char *s) {
+    s[strcspn(s, "\n")] = '\0';
+}
+
+int valid_text(const char *s) {
+    for (int i = 0; s[i] != '\0'; i++) {
+        if ((s[i] < 'A' || s[i] > 'Z') && s[i] != ' ') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    int socketFD, portNumber;
+    struct sockaddr_in serverAddress;
+    struct hostent* serverHostInfo;
+
+    char plaintext[MAX_BUF];
+    char key[MAX_BUF];
+    FILE *fp;
+
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s plaintext key port\n", argv[0]);
+        exit(1);
+    }
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: cannot open %s\n", argv[1]);
+        exit(1);
+    }
+    memset(plaintext, '\0', sizeof(plaintext));
+    fgets(plaintext, sizeof(plaintext), fp);
+    fclose(fp);
+    strip_newline(plaintext);
+
+    fp = fopen(argv[2], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: cannot open %s\n", argv[2]);
+        exit(1);
+    }
+    memset(key, '\0', sizeof(key));
+    fgets(key, sizeof(key), fp);
+    fclose(fp);
+    strip_newline(key);
+
+    if (!valid_text(plaintext) || !valid_text(key)) {
+        fprintf(stderr, "dec_client error: input contains bad characters\n");
+        exit(1);
+    }
+
+    if (strlen(key) < strlen(plaintext)) {
+        fprintf(stderr, "Error: key '%s' is too short\n", argv[2]);
+        exit(1);
+    }
+
+    portNumber = atoi(argv[3]);
+
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    serverHostInfo = gethostbyname("localhost");
+
+    memset((char*)&serverAddress, '\0', sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(portNumber);
+
+    memcpy((char*)&serverAddress.sin_addr.s_addr,
+           (char*)serverHostInfo->h_addr,
+           serverHostInfo->h_length);
+
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        fprintf(stderr, "Error: could not contact enc_server on port %d\n", portNumber);
+        exit(2);
+    }
+
+    char buffer[200000];
+    memset(buffer, '\0', sizeof(buffer));
+
+    sprintf(buffer, "%s\n%s", plaintext, key);
+
+    send(socketFD, buffer, strlen(buffer), 0);
+
+    memset(buffer, '\0', sizeof(buffer));
+    recv(socketFD, buffer, sizeof(buffer) - 1, 0);
+
+    printf("%s\n", buffer);
+
+    close(socketFD);
+    return 0;
+}
---
+++ enc_server.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    int listenSocket, connectionSocket, portNumber;
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t sizeOfClientInfo = sizeof(clientAddress);
+
+    if (argc < 2) {
+        fprintf(stderr, "USAGE: %s port\n", argv[0]);
+        exit(1);
+    }
+
+    portNumber = atoi(argv[1]);
+
+    listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    memset((char *)&serverAddress, '\0', sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(portNumber);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    listen(listenSocket, 5);
+
+    while (1) {
+        while (waitpid(-1, NULL, WNOHANG) > 0);
+
+        connectionSocket = accept(listenSocket,
+                                  (struct sockaddr *)&clientAddress,
+                                  &sizeOfClientInfo);
+
+        if (connectionSocket < 0) {
+            continue;
+        }
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            close(listenSocket);
+
+            char buffer[200000];
+            memset(buffer, '\0', sizeof(buffer));
+
+            recv(connectionSocket, buffer, sizeof(buffer) - 1, 0);
+
+            char *plaintext = strtok(buffer, "\n");
+            char *key = strtok(NULL, "\n");
+
+            char ciphertext[200000];
+            memset(ciphertext, '\0', sizeof(ciphertext));
+
+            for (int i = 0; plaintext[i] != '\0'; i++) {
+                int p, k, c;
+
+                if (plaintext[i] == ' ')
+                    p = 26;
+                else
+                    p = plaintext[i] - 'A';
+
+                if (key[i] == ' ')
+                    k = 26;
+                else
+                    k = key[i] - 'A';
+
+                c = (p + k) % 27;
+
+                if (c == 26)
+                    ciphertext[i] = ' ';
+                else
+                    ciphertext[i] = 'A' + c;
+            }
+
+            send(connectionSocket, ciphertext, strlen(ciphertext), 0);
+            close(connectionSocket);
+            exit(0);
+        } else {
+            close(connectionSocket);
+        }
+    }
+
+    close(listenSocket);
+    return 0;
+}
